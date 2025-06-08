ed6c4454c3d1ff901654a8c43c6d9f337eb95b1c
Benjamin T. Rittgers <brittger@orion.writerbenjamin.com>
2025-06-08 15:09:11 -0500

    created firmware for Marspad, a macropad with two rotary encoders and an oled display.

---
+++ keyboards/writerbenjamin/marspad/keymaps/default/config.h
@@ -0,0 +1,3 @@
+#pragma once
+#define I2C1_SDA_PIN GP19
+#define I2C1_SCL_PIN GP20
---
+++ keyboards/writerbenjamin/marspad/keymaps/default/keymap.c
@@ -0,0 +1,42 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┐       ┌───┐
+     * │ M │       │ _ │
+     * ├───┼───┬───┼───┤
+     * │ 1 │ 2 │ 3 │ 4 │
+     * ├───┼───┼───┼───┤
+     * │ 5 │ 6 │ 7 │ 8 │
+     * ├───┼───┼───┼───┤
+     * │ 9 │ 0 │ A │ B │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_MUTE,                KC_TAB,
+        KC_1,   KC_2,   KC_3,   KC_4,
+        KC_5,   KC_6,   KC_7,   KC_8,
+        KC_9,   KC_0,    KC_A,  KC_B
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+};
+#endif
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
+    };
+    oled_write_P(qmk_logo, false);
+    return false;
+}
+#endif
