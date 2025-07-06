ce5dec6f4b81298c4b1f972184a4a8e55bc5793d
Benjamin T. Rittgers <brittger@orion.writerbenjamin.com>
2025-07-06 08:28:33 -0500

    everything done except the oled

--- keyboards/writerbenjamin/venuspad/keymaps/default/keymap.c
+++ keyboards/writerbenjamin/venuspad/keymaps/default/keymap.c
@@ -3,6 +3,11 @@
 
 #include QMK_KEYBOARD_H
 
+enum layer_names {
+	L1,
+	L2
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      *     ┌───┬───┬───┬───┐
@@ -10,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *     ├───┼───┼───┼───┤
      *     │ 7 │ 8 │ 9 │   │
      * ┌───┼───┼───┼───┤ + │
-     * │MO1│ 4 │ 5 │ 6 │   │
+     * │TG1│ 4 │ 5 │ 6 │   │
      * └───┼───┼───┼───┼───┤
      *     │ 1 │ 2 │ 3 │   │
      *     ├───┴───┼───┤Ent│
@@ -20,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
                   KC_NUM,   KC_PSLS, KC_PAST, KC_PMNS,
                   KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
-        MO(1),    KC_P4,   KC_P5,   KC_P6,
+        TO(1),   KC_P4,    KC_P5,   KC_P6,
                   KC_P1,    KC_P2,   KC_P3,   KC_PENT,
                   KC_P0,    KC_PDOT
     ),
@@ -31,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *     ├───┼───┼───┼───┤
      *     │ 7 │ 8 │ 9 │   │
      * ┌───┼───┼───┼───┤ + │
-     * │ROT│ 4 │ 5 │ 6 │   │
+     * │TG0│ 4 │ 5 │ 6 │   │
      * └───┼───┼───┼───┼───┤
      *     │ 1 │ 2 │ 3 │   │
      *     ├───┴───┼───┤Ent│
@@ -41,28 +46,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
                  _______, _______, _______, _______,
                  _______, _______, _______, _______,
-        _______, _______, _______, _______,
+        TO(0),  _______, _______, _______,
                  _______, _______, _______, _______,
                  _______, _______
     )
 };
 
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        // gpio_write_pin sets the pin high for 1 and low for 0.
+        gpio_write_pin(D4, led_state.num_lock);
+    }
+    return res;
+}
+
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD)  },
+    [1] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP)  },
 };
 #endif
 
 #ifdef OLED_ENABLE
+//oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+//  return OLED_ROTATION_90;   // flips the display 180 degrees if offhand
+
 bool oled_task_user(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
-    };
-    oled_write_P(qmk_logo, false);
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_ln_P(led_state.caps_lock ? PSTR("CAP") : PSTR("   "), false);
+    oled_write_ln_P(led_state.num_lock ? PSTR("NUM") : PSTR("   "), false);
+    oled_write_ln_P(led_state.scroll_lock ? PSTR("SCR") : PSTR("   "), false);
+    uint8_t layer = get_highest_layer(layer_state);
+    switch (layer) {
+	case L1:
+	    oled_write_ln_P(PSTR("L1"), false);
+	    break;
+	case L2:
+	    oled_write_ln_P(PSTR("L2"), false);
+	    break;
+	default:
+	    break;
+    }
     return false;
 }
 #endif
-
